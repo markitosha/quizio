360121caf05651ea134c4c5802059624c222e7f1
n.markitantova <n.markitantova@tinkoff.ru>
2023-03-02 23:10:20 +0300

    add linter

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint/eslint-plugin"],
+  overrides: [
+    {
+      files: ["frontend/**"],
+      parserOptions: {
+        project: ["./frontend/tsconfig.json"],
+      },
+    },
+    {
+      files: ["backend/**"],
+      parserOptions: {
+        project: ["./backend/tsconfig.json"],
+      },
+    },
+  ],
+  rules: {
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+};
